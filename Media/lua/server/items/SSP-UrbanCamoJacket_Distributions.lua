9d20eed84c370d321b5e8d65ece59879b30e6a7f
SenorSmartyPants <senorsmartypants@gmail.com>
2023-02-13 14:54:25 -0600

    Version 0.1.0

---
+++ Media/lua/server/items/SSP-UrbanCamoJacket_Distributions.lua
@@ -0,0 +1,11 @@
+require 'Items/ProceduralDistributions'
+local i, j, d
+
+table.insert(ProceduralDistributions.list.ArmySurplusOutfit.items, "Jacket_ArmyCamoUrban");
+table.insert(ProceduralDistributions.list.ArmySurplusOutfit.items, 6);
+
+table.insert(ProceduralDistributions.list.LockerArmyBedroom.items, "Jacket_ArmyCamoUrban");
+table.insert(ProceduralDistributions.list.LockerArmyBedroom.items, 4);
+
+table.insert(ProceduralDistributions.list.WardrobeRedneck.items, "Jacket_ArmyCamoUrban");
+table.insert(ProceduralDistributions.list.WardrobeRedneck.items, 0.5);
